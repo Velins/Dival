843903235c1c05de6e31bd4b7e3316d178b6204c
Valenstoune <Email@gmail.com>
2024-04-25 20:54:30 +0300

    added debug-toolbar

--- Dival/urls.py
+++ Dival/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from Dival.settings import DEBUG
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
     path('catalog/', include('products.urls', namespace='catalog')),
 ]
+
+if DEBUG:
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls")),
+    ]
