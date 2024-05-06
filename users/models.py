644befdb4bb3f0596089840348326802747efe75
Valenstoune <Email@gmail.com>
2024-05-06 18:14:23 +0300

    Login form + controller

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser 
 
-# Create your models here.
+class User(AbstractUser):
+    profile_image = models.ImageField(upload_to='users_images', blank=True, null=True, verbose_name='Аватар')
+
+    class Meta:
+        db_table = 'Users'
+        verbose_name =  'Користувач'
+        verbose_name_plural = 'Користувачі'
+
+    def __str__(self):
+        return self.username
--- users/views.py
+++ users/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render
-
+from django.contrib import auth
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from users.forms import UserLoginForm
 # Create your views here.
 
-def login (request):
+# Ваші імпорти та інші в'юшки
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username = username, password = password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserLoginForm()
+
     context = {
-        'title' : 'DiVal - Авторизація'
+        'title': 'Login',
+        'form': form
     }
-    return render(request,'users/login.html', context)
+    return render(request, 'users/login.html', context)
+
 
 def registration (request):
     context = {
