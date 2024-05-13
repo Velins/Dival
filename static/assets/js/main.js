91e1e3326ddf88fe0bbba925907ffea245e5ccec
Valenstoune <Email@gmail.com>
2024-05-13 19:46:00 +0300

    Add change cart

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -83,6 +83,74 @@ $(document).ready(function() {
             },
         });
     });
+
+    $(document).on("click", ".decrement", function () {
+
+        var url = $(this).data("cart-change-url");
+
+        var cartID = $(this).data("cart-id");
+
+        var $input = $(this).closest('.ch-qty').find('.quantity');
+
+        var currentValue = parseInt($input.text());
+
+        if (currentValue > 1) {
+            $input.val(currentValue - 1);
+
+            updateCart(cartID, currentValue - 1, -1, url);
+        }
+    });
+    
+    $(document).on("click", ".increment", function () {
+
+        var url = $(this).data("cart-change-url");
+
+        var cartID = $(this).data("cart-id");
+
+        var $input = $(this).closest('.ch-qty').find('.quantity');
+
+        var currentValue = parseInt($input.text());
+
+        $input.val(currentValue + 1);
+
+
+        updateCart(cartID, currentValue + 1, 1, url);
+    });
+
+    function updateCart(cartID, quantity, change, url) {
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: {
+                cart_id: cartID,
+                quantity: quantity,
+                csrfmiddlewaretoken: $("[name=csrfmiddlewaretoken]").val(),
+            },
+
+            success: function (data) {
+
+                successMessage.html(data.message);
+                successMessage.fadeIn(400);
+
+                setTimeout(function () {
+                     successMessage.fadeOut(400);
+                }, 2000);
+
+
+                var productsInCartCount = $("#products-in-cart-count");
+                var cartCount = parseInt(productsInCartCount.text() || 0);
+                cartCount += change;
+                productsInCartCount.text(cartCount);
+
+                var cartItemsContainer = $("#cart-items-container");
+                cartItemsContainer.html(data.cart_items_html);
+
+            },
+            error: function (data) {
+                console.log("Помилка додавання товару в корзину !");
+            },
+        });
+    }
 });
 
 
