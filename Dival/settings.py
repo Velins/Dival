32bc8dcc753dd23ae9fdcf05823e0cd233dcade9
Valenstoune <Email@gmail.com>
2024-06-10 20:54:55 +0300

    Deploy

--- Dival/settings.py
+++ Dival/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-urv(8t&2wt%r%b+wbeiwtmyk9i^(qb0!y$5o1isrkho@5wht(^'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -96,7 +98,12 @@ DATABASES = {
         'PORT': '5432',
     }
 }
- 
+
+database_url = os.environ.get("DATABASE_URL")
+
+DATABASES['default'] = dj_database_url.parse(database_url)
+
+# postgres://admindival:yYEFc9V47Ku1I9mW4eem1hG1p9tPiDgg@dpg-cpjja47109ks73en87hg-a.frankfurt-postgres.render.com/dival 
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from phonenumber_field.modelfields import PhoneNumberField
+from .managers import CustomUserManager
 
 class User(AbstractUser):
     username = None
@@ -9,6 +10,8 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
     
     class Meta:
         db_table = 'Users'
