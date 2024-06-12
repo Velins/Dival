4838d74abde8b503e038da23d992effe73b01dc5
Velinston <Lobchuk45@gmail.com>
2024-06-12 22:47:01 +0300

    change "staticfiels" to "static"

--- Dival/wsgi.py
+++ Dival/wsgi.py
@@ -17,5 +17,4 @@ from Dival.settings import BASE_DIR
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Dival.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
-
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'static'))
