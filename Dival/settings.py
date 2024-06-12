601b25ad25ef2fc69152b66b15b7c9c04d5dc39f
Velinston <Lobchuk45@gmail.com>
2024-06-12 22:25:44 +0300

    Add whitenoise for static files

--- Dival/settings.py
+++ Dival/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -141,9 +142,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-    ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static',
+#     ]
 
 MEDIA_URL = 'media/'
 
