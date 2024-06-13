10c06078f2aad1d6dccc4b79f8ae8fcfdf0d8e38
Velinston <Lobchuk45@gmail.com>
2024-06-13 15:30:21 +0300

    add in wsgi

--- Dival/wsgi.py
+++ Dival/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
+
+from Dival.settings import BASE_DIR
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Dival.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
